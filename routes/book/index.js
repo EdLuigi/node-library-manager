0ed1ba11680c1195e9c66a3bc2a10a3541cdf486
EdLuigi <luigi.ciuffi@hotmail.com>
2023-02-28 20:55:48 -0300

    rota POST de book (falta conectar com mongo)

--- routes/book/index.js
+++ routes/book/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const Book = require("../../model/book");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -8,6 +9,27 @@ router.get("/", function (req, res, next) {
 
 router.get("/add-book", function (req, res, next) {
     res.render("form/add-book");
+});
+
+router.post("/add-book", async (req, res) => {
+    console.log(req.body);
+
+    // const book = new Book(req.body);
+    // book.save(function (err, result) {
+    //     if (err) throw err;
+    //     return res.json(result);
+    // });
+
+    try {
+        const book = await Book.create(req.body);
+        res.json(result);
+    } catch (error) {
+        console.log("error: " + error.toString());
+    } finally {
+        res.redirect("/book/add-book");
+    }
+
+    // res.json({ message: "book added successfully!" });
 });
 
 module.exports = router;
